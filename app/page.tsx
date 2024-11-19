5151d3bae7f9ce88760c67e540f3480383d673e1
Shravan Joshi <shravanjoshihydi@gmail.com>
2024-11-19 23:01:52 +0530

    ESlint and prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-const Home = () =>  (
-    <div className="text-3xl text-white font-black">Welcome to the world of nextjs</div>
-  );
+const Home = () => (
+  <div className="text-3xl font-black text-white">
+    Welcome to the world of nextjs
+  </div>
+);
 
 export default Home;
